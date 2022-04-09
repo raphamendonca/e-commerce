0a01b568fe9f5372c705ad2788bb6826ae827290
moisessosa <rod20028@byui.com>
2022-04-08 23:25:14 -0300

    adicionei Usuario no pacote entity

---
+++ src/main/java/br/com/letscode/ecommerce/domain/model/entity/Usuario.java
@@ -0,0 +1,64 @@
+package br.com.letscode.ecommerce.domain.model.entity;
+
+import lombok.AccessLevel;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Collection;
+import java.util.List;
+
+@Entity(name ="usuario")
+public class Usuario implements UserDetails {
+    @Id
+    @Setter(AccessLevel.NONE)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(()->"read");
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
